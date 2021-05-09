fd7e9b713e9f61aab8875b1bd3ef06eea46098dd
Aldo Praherda <aldo@aldoapp.com>
2021-05-09 22:24:31 +0700

    updated

---
+++ static/absoluteyoga/FE/assets/js/custom.js
@@ -0,0 +1,29 @@
+$("#buttonarrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
